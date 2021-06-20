28040ea17d20c33ad5c62e353f2878040d67bebc
James Bielicke <j.bielicke@ufl.edu>
2021-06-20 15:57:24 -0500

    Add working on js section Trying to figure out the loop and event listener

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 // Create an Array of at least 3 losing messages
-var loser = [
-"Nope. Not even close.",
-"Are you dead from the neck up?",
-"Do you even have a pulse?"
-];
+
+    var loser = [
+        "Nope. Not even close.",
+        "Hello? Anybody home?",
+        "Do you even have a pulse?"
+    ];
+
+    //console.log(loser);
+
 
 // Create variables to count wins and losses
 
+    var winsTotal = 0
+    var losesTotal = 0
+
+    //console.log(winsTotal);
+    //console.log(losesTotal);
+
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
+
+    var message = document.getElementById('message');
+    var wins = document.getElementById('wins');
+    var losses = document.getElementById('losses');
+
+    //console.log(message);
+    //console.log(wins);
+    //console.log(losses);
 
 // target all .box elements and attach a click event listener to each one using a loop
 
 // within each click event...
 
+    var box = document.getElementsByClassName('box');
+    console.log(box);
+
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
+
+    for (i=0; i<box.length; i++) {
+        box[i].addEventListener('click', function) {
+
+        };
+
+    }
+
 
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
