ba19953fde81c15f2683c30967bcc373a9956701
James Bielicke <j.bielicke@ufl.edu>
2021-06-20 22:08:56 -0500

    Add final JS coding & media queries Unable to get click event to work. Finished CSS & media queries

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
     var loser = [
         "Nope. Not even close.",
         "Hello? Anybody home?",
-        "Do you even have a pulse?"
-    ];
+        "Are you even trying?"
+    ]
 
     //console.log(loser);
 
@@ -12,7 +12,7 @@
 // Create variables to count wins and losses
 
     var winsTotal = 0
-    var losesTotal = 0
+    var lossesTotal = 0
 
     //console.log(winsTotal);
     //console.log(losesTotal);
@@ -29,28 +29,46 @@
 
 // target all .box elements and attach a click event listener to each one using a loop
 
-// within each click event...
+    var boxOpts = document.getElementsByClassName('.box');
+    //console.log(boxOpts);
 
-    var box = document.getElementsByClassName('box');
-    console.log(box);
+    //this is where I started to get lost and derailed
+    for (i=0; i<boxOpts.length; i++) {
+        boxOpts[i].addEventListener('click');
+        box.onclick = clickIt;
+    }
 
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
 
-    for (i=0; i<box.length; i++) {
-        box[i].addEventListener('click', function) {
-
-        };
-
-    }
-
+    function clickIt(event) {
+        var boxMain = parseInt(this.textContent); //this seemed ok
+    
 
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
+
+    var randomNum = Math.floor(Math.random() * 4); //this seemed ok
+    //console.log(randomNum)
+
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
 // if the numbers match, increment wins and display the win count in div#wins
 
+    if (randomNum === boxMain) { //this seemed ok
+        displayedmessage.textContent = "It's your lucky day!"
+        allWins += 1
+        displayedwincount.textContent = "Wins: " + allWins
+    }
+
+
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
+
+    else { //this seemed ok
+        function loseMess(loser) {
+            return loser[Math.floor(Math.random() * loser.length)]
+        }
+        lossesTotal.textContent = loser 
+    }
